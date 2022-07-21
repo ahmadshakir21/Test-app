788a6c3ffc818bba1ac7e134bd1eaf6b55c49a72
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-07-21 13:28:53 +0300

    Commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 <h3>Food Ordering App</h3>
 
-<img src=https://unblast.com/wp-content/uploads/2020/08/Food-Ordering-App-Template.jpg></img>
+this app is used for deliverying food
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -11,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
@@ -29,25 +40,125 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final nameController = TextEditingController();
+  final authorController = TextEditingController();
+  final priceConttroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    CollectionReference bookRef =
+        FirebaseFirestore.instance.collection("books");
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ListView.builder(
-            itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.all(5),
-                  child: Card(
-                    color: Colors.pink.shade200,
-                    child: const ListTile(
-                      title: Center(
-                          child: Text("Title",
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold))),
-                      subtitle: Center(child: Text("SubTitle")),
+      appBar: AppBar(
+        title: const Text("Flutter & Firebase"),
+      ),
+      body: Center(
+        child: StreamBuilder(
+            stream: bookRef.snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot<Object?>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else {
+                return ListView(
+                    children: snapshot.data!.docs.map((book) {
+                  final dynamic data = book.data();
+                  return Center(
+                    child: ListTile(
+                      title: Text(data['title'] ?? ""),
+                      subtitle: Text(data['author'] ?? ""),
+                      trailing: Text(data['price'] ?? ""),
                     ),
-                  ),
-                )));
+                  );
+                }).toList());
+              }
+            }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Center(
+            child: Icon(
+          Icons.add,
+          size: 50,
+        )),
+        onPressed: () => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text("Adding Book"),
+            content:
+                const Text("Filling this below information for adding a book"),
+            actions: [
+              Row(
+                children: [
+                  Container(
+                      padding: const EdgeInsets.all(10),
+                      width: 70,
+                      child: const Text("Name : ")),
+                  Expanded(
+                    child: Container(
+                      width: 220,
+                      height: 30,
+                      child: TextField(
+                          controller: nameController,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5)))),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Container(
+                      padding: const EdgeInsets.all(10),
+                      width: 70,
+                      child: const Text("Author : ")),
+                  Expanded(
+                    child: Container(
+                      width: 220,
+                      height: 30,
+                      child: TextField(
+                          controller: authorController,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5)))),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Container(
+                      padding: const EdgeInsets.all(10),
+                      width: 70,
+                      child: const Text("Price : ")),
+                  Expanded(
+                    child: Container(
+                      width: 220,
+                      height: 30,
+                      child: TextField(
+                          controller: priceConttroller,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5)))),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                  onPressed: () {
+                    bookRef.add({
+                      'title': nameController.text,
+                      'author': authorController.text,
+                      'price': priceConttroller.text
+                    });
+                  },
+                  child: const Text("Add"))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
